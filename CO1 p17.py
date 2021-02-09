6add02cb3f7753fd2ed05aabba051ea429a55331
athiathira <76744021+athiathira@users.noreply.github.com>
2021-02-09 17:52:17 +0530

    Add files via upload

---
+++ CO1 p17.py
@@ -0,0 +1,11 @@
+y={'appu':40,'alan':2,'bob':1,'danny':3} 
+                                         
+l=list(y.items())   
+l.sort()            
+print('Ascending order is',l) 
+l=list(y.items())
+l.sort(reverse=True) 
+print('Descending order is',l)
+
+dict=dict(l) 
+print("Dictionary",dict) 
